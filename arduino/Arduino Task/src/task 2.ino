a2d5f6eb128c929965f6f986949af24bd897a3e3
m.m.helmi1-25@student.lboro.ac.uk <m.m.helmi1-25@student.lboro.ac.uk>
2026-04-18 00:11:03 +0100

    - Creation of array_length const to prevent recalculation / adjusted to collecting temp code
    - perform_dft function created and calculations, with print statementsfor debugging

--- arduino/Arduino Task/src/task 2.ino
+++ arduino/Arduino Task/src/task 2.ino
@@ -5,11 +5,12 @@ const int B = 4275000; // B value of the thermistor
 const int R0 = 100000; // R0 = 100k
 const int pinTempSensor = A0; // Grove - Temperature Sensor connect to A0
 
-int index = 0;
-float array_of_temps[180];
+float array_of_temps[180]; // Array length 180 because it reads temperature every second for 3 minutes (180 seconds)
+const int array_length = (sizeof(array_of_temps) / sizeof(array_of_temps[0]));
 
 int collect_temperature_data(float array_of_temps[], int index);
 int save_to_array(float temperature, float array_of_temps[], int index);
+int perform_dft(float array_of_temps[], int array_length);
 
 void setup()
 {
@@ -18,12 +19,15 @@ void setup()
 
 void loop()
 {
-  while (index <= (sizeof(array_of_temps) / sizeof(array_of_temps[0])))
+  // Collect temperature data every second for 3 minutes (180 seconds)
+  for (int index = 0; index < array_length; index++)
   {
     collect_temperature_data(array_of_temps, index);
-    index++;
     delay(1000);
   }
+
+  // Perform DFT on the collected temperature data
+  apply_dft(array_of_temps, array_length);
 }
 
 int collect_temperature_data(float array_of_temps[], int index)
@@ -47,3 +51,37 @@ int save_to_array(float temperature, float array_of_temps[], int index)
     array_of_temps[index] = temperature;
     return 0;
 }
+
+int apply_dft(float array_of_temps[], int array_length)
+{
+  const float fs = 1.0; // sampling frequency in Hz
+  float real[array_length];
+  float imag[array_length];
+  float frequency[array_length];
+  float magnitude[array_length];
+
+  for (int k = 0; k < array_length; k++) {
+    real[k] = 0.0;
+    imag[k] = 0.0;
+
+    for (int n = 0; n < array_length; n++) {
+      float angle = 2.0 * PI * k * n / array_length;
+      real[k] += array_of_temps[n] * cos(angle);
+      imag[k] -= array_of_temps[n] * sin(angle);
+    }
+
+    float magnitude[k] = sqrt(real[k] * real[k] + imag[k] * imag[k]);
+    float frequency[k] = (k * fs) / array_length;
+
+    Serial.print("DFT[");
+    Serial.print(k);
+    Serial.print("] f=");
+    Serial.print(frequency[k], 4);
+    Serial.print(" Hz magnitude=");
+    Serial.println(magnitude[k], 4);
+  }
+
+  return frequency;
+}
+
+
