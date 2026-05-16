730cf95ed281f8d1533848bfcf736295982ed662
m.m.helmi1-25@student.lboro.ac.uk <m.m.helmi1-25@student.lboro.ac.uk>
2026-05-16 18:51:41 +0100

    - Added code to plot both graphs - Addred plot_graphs function to organise the plots into a single figure

--- task4_data_analysics/process_data_to_graphs.py
+++ task4_data_analysics/process_data_to_graphs.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 FILE = "task4_data_analysics\\temperatures.csv"
 
 def read_file(FILE):
@@ -22,14 +24,54 @@ def split_data(data):
         frequency.append(float(row[2]))
         magnitude.append(float(row[3]))
 
-    return time, temperature, magnitude, frequency
+    return time, temperature, frequency, magnitude
 
+def plot_graphs(time, temperature, frequency, magnitude):
+    plt.figure(figsize=(12, 10))
+
+    # Draw plot 1
+    plot_1(time, temperature)
+
+    # Draw plot 2
+    plot_2(frequency, magnitude)
+    
+    plt.tight_layout()
+    plt.show()
+
+def plot_1(time, temperature):
+    plt.subplot(2, 1, 1)
+
+    plt.plot(time, temperature)
+
+    plt.title("Temperature vs Time")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Temperature (°C)")
+    plt.grid(True)
+
+
+def plot_2(frequency, magnitude):
+    plt.subplot(2, 1, 2)
+
+    plt.plot(frequency, magnitude)
+
+    plt.title("Magnitude vs Frequency")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Magnitude")
+    plt.grid(True)
+
+
+# Read data from the file
 data = read_file(FILE)
 #print(data)
 
-time, temperature, magnitude, frequency = split_data(data)
+# Split data into separate lists
+time, temperature, frequency, magnitude = split_data(data)
 
 #print("Time:", time)
 #print("Temperature:", temperature)
-#print("Magnitude:", magnitude)
 #print("Frequency:", frequency)
+#print("Magnitude:", magnitude)
+
+# Plot all graphs
+plot_graphs(time, temperature, frequency, magnitude)
+
